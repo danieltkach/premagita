d1360d8bf05b614c256871c46d12fddc03090cad
Daniel Tkach <danieltkach@gmail.com>
2021-04-20 12:29:46 -0300

    project structure full refactor

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const app = require("express").express();
+const PORT = 3001;
+
+app.get("/api", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${port}...`);
+});
---
+++ server/config/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const config = {
+  app: {
+    port: process.env.PORT,
+  },
+  database: {
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    name: process.env.DATABASE_NAME,
+    connectionString: process.env.CONNECTION_STRING,
+  },
+  jwt: {},
+};
+
+export default config;
