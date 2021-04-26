ae9c9ba867ca39ce3d348697ffd9a6909cc6d7f6
Daniel Tkach <danieltkach@gmail.com>
2021-04-26 09:55:10 -0300

    basic styling

--- client/src/components/SettingsScreen.jsx
+++ client/src/components/SettingsScreen.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Editor from "./Editor";
+import styles from "../css/SettingsScreen.module.css";
 
 const SettingsScreen = () => {
   return (
-    <>
+    <div className={styles.container}>
       <h1>Configuración</h1>
       <Editor />
-    </>
+    </div>
   );
 };
 
